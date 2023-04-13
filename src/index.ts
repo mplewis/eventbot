b3986e11bb48ea4192a494f6a0c9b56b470dd0a6
Matt Lewis <matt@mplewis.com>
2023-04-13 14:10:26 -0700

    feat: receive tagged messagse

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,31 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import { Client, ClientUser, Events, GatewayIntentBits } from "discord.js";
 import * as dotenv from "dotenv";
 import { pino } from "pino";
 
-const log = pino();
+const glog = pino();
 
 dotenv.config();
 
 const token = process.env.DISCORD_BOT_TOKEN;
 if (!token) throw new Error("missing bot token");
 
+let me: ClientUser | undefined;
 const client = new Client({ intents: ["Guilds", "GuildMessages"] });
 
 client.once(Events.ClientReady, (c) => {
-	log.info("Ready!", { user: c.user.tag });
+	let log = glog.child({ id: c.user.id, tag: c.user.tag });
+	me = c.user;
+	log.info("Ready!");
 });
 
-log.info("Connecting...");
+glog.info("Connecting...");
 client.login(token);
 
 client.on("messageCreate", (m) => {
-	log.info(m);
-	if (m.content.includes("ping")) m.reply("pong");
+	if (!m.content) return;
+	if (m.author.id === me?.id) return;
+	const tagMatcher = /(<@\d+>\s*)/g;
+	const content = m.content.replaceAll(tagMatcher, "");
+	let log = glog.child({ from: m.author.id, me: me?.id, content });
+	log.info("Received message");
 });
