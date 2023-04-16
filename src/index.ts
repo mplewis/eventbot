02b63cf60bc09c5e851c7cc11d6b397696189dc1
Matt Lewis <matt@mplewis.com>
2023-04-15 20:54:42 -0600

    feat: initial interaction handling

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { Client, ClientUser, Events } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Client, ClientUser, Events } from "discord.js";
 import dayjs from "dayjs";
 import { discordBotToken } from "./env";
 import { glog } from "./log";
 import { parseCreateEvent } from "./openai";
-import { ppEvent } from "./pretty";
+import { parsePPEvent, ppEvent } from "./pretty";
 
 let me: ClientUser | undefined;
 const client = new Client({ intents: ["Guilds", "GuildMessages"] });
@@ -40,6 +40,24 @@ client.on("messageCreate", async (m) => {
 		return;
 	}
 	const msg = ppEvent(resp.result, content);
-	await m.channel.send(msg);
+	const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+		new ButtonBuilder().setLabel("Create Event").setStyle(ButtonStyle.Success).setCustomId("createEvent"),
+		new ButtonBuilder().setLabel("Edit Details").setStyle(ButtonStyle.Primary).setCustomId("editDetails"),
+		new ButtonBuilder().setLabel("Delete Event").setStyle(ButtonStyle.Danger).setCustomId("deleteEvent")
+	);
+	await m.channel.send({ content: msg, components: [row] });
 	await loading.delete();
+});
+
+client.on("interactionCreate", async (intn) => {
+	if (!intn.isButton()) return;
+	glog.debug(intn);
+
+	const parsed = parsePPEvent(intn.message.content);
+	glog.debug(parsed);
+
+	(async () => {
+		const resp = await intn.reply({ content: `\`${intn.customId}\` not implemented yet.`, ephemeral: true });
+		setTimeout(() => resp.delete(), 2000);
+	})();
 });
