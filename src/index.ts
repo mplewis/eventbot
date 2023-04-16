da697259b3ec6abdb244f0d24f0778260a8514e5
Matt Lewis <matt@mplewis.com>
2023-04-15 20:27:14 -0600

    feat: loading msg + parse create + response; tidy: no debug'

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,18 @@
 import { Client, ClientUser, Events } from "discord.js";
 import dayjs from "dayjs";
 import { discordBotToken } from "./env";
-import { promptCreateEvent } from "./template";
 import { glog } from "./log";
-import { parseCreateEvent, parseEditEvent } from "./openai";
+import { parseCreateEvent } from "./openai";
+import { ppEvent } from "./pretty";
 
 let me: ClientUser | undefined;
 const client = new Client({ intents: ["Guilds", "GuildMessages"] });
-
-const eventInfo = `
-Hey All! I'm organizing a get together for anyone who wants to come out east (or is already out here).  The Parkside Eatery located at 14515 E Alameda Ave, Aurora, CO 80012, has an open area for seating, a bar, and tons of restaurants including:
-- Gina's Kitchen - an allergy sensitive restaurant with vegan as well a gluten free options
-- Taco Choi
-- The Lucky Bird
-- Halal Guys
-- Five Guys
-- Cheba Hut (toasted subs)
-- Playa Bowls
-It's also right down the street from In and Out Burger and the Aurora Metro Center for the public transportation users.
-
-This will be a fun, casual, family friendly get together.  Everyone is welcome.  Come have fun!
-`;
-
-const eventInfo2 = `LFG for Ragnaros, need a tank and a healer.  I'm a 60 mage.`;
 
 client.once(Events.ClientReady, (c) => {
 	let log = glog.child({ id: c.user.id, tag: c.user.tag });
 	me = c.user;
 	log.info("Ready!");
 });
-
-(async () => {
-	const dateWithTZ = dayjs().format("MMMM D, YYYY, h:mm A z");
-	// const result = await parseCreateEvent({ dateWithTZ, eventInfo: eventInfo });
-	const result = await parseEditEvent({
-		dateWithTZ,
-		existingEventData: {
-			name: "Aurora Meetup",
-			date: "2021-09-25T14:00:00.000-07:00",
-			location: "Parkside Eatery",
-			url: null,
-		},
-		// updateInfo: "Event date changed to Sep 25, 4-7pm MDT",
-		updateInfo: eventInfo2,
-	});
-	glog.info(result);
-})();
 
 glog.info("Connecting...");
 client.login(discordBotToken);
@@ -58,8 +25,21 @@ client.on("messageCreate", async (m) => {
 	let log = glog.child({ from: m.author.id, me: me?.id, content });
 	log.info("Received message");
 
+	const loading = await m.channel.send("Thinking...");
+
 	const dateWithTZ = dayjs().format("MMMM D, YYYY, h:mm A z");
-	const prompt = await promptCreateEvent({ dateWithTZ, eventInfo: content });
-	const compl = await complete(prompt);
-	m.channel.send(compl);
+	const utcOffset = dayjs().format("Z");
+	const resp = await parseCreateEvent({ dateWithTZ, utcOffset, eventInfo: content });
+	if ("error" in resp) {
+		log.error(resp.error);
+		m.channel.send("Sorry, something went wrong.");
+		return;
+	}
+	if ("irrelevant" in resp) {
+		m.channel.send("Sorry, that didn't look like an event to me.");
+		return;
+	}
+	const msg = ppEvent(resp.result, content);
+	await m.channel.send(msg);
+	await loading.delete();
 });
